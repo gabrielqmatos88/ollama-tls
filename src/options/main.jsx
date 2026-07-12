bcbac00dc7dc088baf52da9f6ab125b2b2f116c9
Gabriel Q. Matos <gabriel.q.matos@gmail.com>
2026-07-12 10:49:17 -0400

    feat: integrate theme in options page entry point

--- src/options/main.jsx
+++ src/options/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { applyTheme } from '@/utils/theme'
 import App from './App.jsx'
 import './App.css'
+
+await applyTheme()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
